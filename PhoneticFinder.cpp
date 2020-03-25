7aec7268bc3a3680d925217e48f694f1de799a28
dvirs12345 <dvirs120@gmail.com>
2020-03-25 22:49:25 +0200

    1.0

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,13 @@
+// Author - Dvir Sadon
+#include "PhoneticFinder.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace phonetic
+{
+	string find(string text, string word)
+    {
+        return "abc";
+    } 
+}
